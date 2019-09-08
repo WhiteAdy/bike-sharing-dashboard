84934628cbf89f752ebe23ffdabdb8920a355eec
WhiteAdy <adi.tamimi@live.com>
2019-09-08 21:28:18 +0300

    Added AJAX DOM mainuplation

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -4,40 +4,82 @@ const insertActiveButton = document.getElementById('insertActive');
 const insertFinishedButton = document.getElementById('insertFinished');
 const removeActiveButton = document.getElementById('removeActive');
 const handlerTestButton = document.getElementById('handlerTest');
+const body = document.querySelector('body');
 
 const basePath = window.location.protocol + '//' + window.location.host;
 
-fetch(`${basePath}/sessions-finished`)
-	.then(res => res.json())
-	.then(data => {
-		console.log(data);
-		data.forEach(document => {
-			sessionsFinishedTable.children[0].children[1].innerHTML += `
-            <tr>
-				<td>${document._id}</td>
-				<td>${document.user}</td>
-				<td>${document.start}</td>
-				<td>${document.end}</td>
-				<td>$${document.fee}</td>
-			</tr>
-            `;
-		});
-	});
+let array_sessionsActive = [];
+let array_sessionsFinished = [];
 
-fetch(`${basePath}/sessions-active`)
-	.then(res => res.json())
-	.then(data => {
-		console.log(data);
-		data.forEach(document => {
-			sessionsActiveTable.children[0].children[1].innerHTML += `
-            <tr>
-				<td>${document._id}</td>
-				<td>${document.user}</td>
-				<td>${document.start}</td>
-			</tr>
-            `;
+let isEqual = (arrayA, arrayB) => {
+	// if length is not equal
+	if (arrayA.length != arrayB.length) return false;
+	else {
+		// comapring each element of array
+		for (var i = 0; i < arrayA.length; i++)
+			if (arrayA[i] != arrayB[i]) return true;
+		return true;
+	}
+};
+
+let flashScreen = () => {
+	body.classList.add('flash');
+	setTimeout(() => {
+		body.classList.remove('flash');
+	}, 150);
+};
+
+let fetchSessionsFinished = () => {
+	fetch(`${basePath}/sessions-finished`)
+		.then(res => res.json())
+		.then(data => {
+			if (!isEqual(array_sessionsFinished, data)) {
+				flashScreen();
+				array_sessionsFinished = data;
+				sessionsFinishedTable.children[0].children[1].innerHTML = '';
+				data.forEach(document => {
+					sessionsFinishedTable.children[0].children[1].innerHTML += `
+				<tr>
+					<td>${document._id}</td>
+					<td>${document.user}</td>
+					<td>${document.start}</td>
+					<td>${document.end}</td>
+					<td>$${document.fee}</td>
+				</tr>
+				`;
+				});
+			}
 		});
-	});
+};
+
+let fetchSessionsActive = () => {
+	fetch(`${basePath}/sessions-active`)
+		.then(res => res.json())
+		.then(data => {
+			if (!isEqual(array_sessionsActive, data)) {
+				flashScreen();
+				array_sessionsActive = data;
+				sessionsActiveTable.children[0].children[1].innerHTML = '';
+				data.forEach(document => {
+					sessionsActiveTable.children[0].children[1].innerHTML += `
+				<tr>
+					<td>${document._id}</td>
+					<td>${document.user}</td>
+					<td>${document.start}</td>
+				</tr>
+				`;
+				});
+			}
+		});
+};
+
+fetchSessionsFinished();
+fetchSessionsActive();
+
+setInterval(() => {
+	fetchSessionsActive();
+	fetchSessionsFinished();
+}, 5000);
 
 insertActiveButton.addEventListener('click', () => {
 	fetch(`${basePath}/sessions-active-add`, {
